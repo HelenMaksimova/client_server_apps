9d53e180ba679360115369ddad10198a77522e09
helen <alnorda@gmail.com>
2021-11-01 01:50:40 +0300

    lesson_05

---
+++ lesson_05/custom_exceptions.py
@@ -0,0 +1,13 @@
+class PortOutOfRange(Exception):
+    def __str__(self):
+        return 'В качестве порта может быть указано только число в диапазоне от 1024 до 65535'
+
+
+class NoResponseInServerMessage(Exception):
+    def __str__(self):
+        return 'Получено некорректное сообщение от сервера (отсутствует поле "response")'
+
+
+class IncorrectData(Exception):
+    def __str__(self):
+        return 'Получены некорректные данные'
